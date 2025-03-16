66ae23d0a2ba2dc98ea3ef6fc2816379802c9281
nowm <nowm@users.noreply.github.com>
2025-03-16 19:15:44 +0300

    Improve build

--- build.ts
+++ build.ts
@@ -1,21 +1,84 @@
-import type {BuildConfig} from 'bun';
+import {BuildConfig} from 'bun';
 import dts from 'bun-plugin-dts';
+import {rm} from 'node:fs/promises';
+import process from 'node:process';
+import packageInfo from './package.json';
 
 const defaultBuildConfig: BuildConfig = {
   entrypoints: ['./src/index.ts'],
-  outdir: './dist'
+  outdir: './dist',
+  minify: process.env.NODE_ENV !== 'development',
 };
 
+type PackageJson = Partial<Omit<typeof packageInfo, 'type' | 'types'> & {
+  type: 'module' | 'commonjs';
+  types: string;
+}>;
+
+const distInfo: PackageJson = {
+  name: packageInfo.name,
+  version: packageInfo.version,
+  description: packageInfo.description,
+  keywords: packageInfo.keywords,
+  homepage: packageInfo.homepage,
+  bugs: packageInfo.bugs,
+  license: packageInfo.license,
+  author: packageInfo.author,
+  exports: {
+    'types': './index.d.ts',
+    'import': './index.js',
+    'require': './index.cjs',
+  },
+  repository: packageInfo.repository,
+  type: 'module',
+  types: './index.d.ts',
+};
+
+const currentYear = new Date().getFullYear();
+let year: number|string = 2025;
+if (year !== currentYear) {
+  year = `${year}-${currentYear}`;
+}
+
+const licenseInfo = `/*
+ * servelat
+ *
+ * Copyright ${year} nowm
+ */
+`;
+
+async function buildJs() {
+  await Promise.all([
+    Bun.build({
+      ...defaultBuildConfig,
+      plugins: [dts()],
+      format: 'esm',
+      naming: "[dir]/[name].js",
+    }),
+    Bun.build({
+      ...defaultBuildConfig,
+      format: 'cjs',
+      naming: "[dir]/[name].cjs",
+    }),
+  ]);
+
+  const files = [
+    './dist/index.js',
+    './dist/index.d.ts',
+    './dist/index.cjs',
+  ];
+
+  for (const path of files) {
+    const content = await Bun.file(path).text();
+    await Bun.write(path, licenseInfo + content);
+  }
+}
+
+await rm('./dist', {recursive: true, force: true});
+
 await Promise.all([
-  Bun.build({
-    ...defaultBuildConfig,
-    plugins: [dts()],
-    format: 'esm',
-    naming: "[dir]/[name].js",
-  }),
-  Bun.build({
-    ...defaultBuildConfig,
-    format: 'cjs',
-    naming: "[dir]/[name].cjs",
-  })
+  buildJs(),
+  Bun.write('./dist/README.md', Bun.file('./README.md')),
+  Bun.write('./dist/LICENSE', Bun.file('./LICENSE')),
+  Bun.write('./dist/package.json', JSON.stringify(distInfo, null, 2)),
 ]);
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
+/*
+ * servelat
+ *
+ * Copyright 2025 nowm
+ */
+
 export const servelat = 'servelat'
-
-export function f() {
-  console.log('f');
-}
-
-export function f2() {
-  console.log('f2');
-}
-
-export function f3() {
-  console.log('f2');
-}
